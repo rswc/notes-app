03eb0482f93fd5365aabc475aa19c90affd2155b
rswc <rshoryti@protonmail.com>
2023-05-12 19:01:47 +0200

    Add shared notes viewing

--- mysite/notes/urls.py
+++ mysite/notes/urls.py
@@ -4,6 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('shared/<int:note_id>', views.shared, name='shared'),
+
+    # actual routing is handled by vue
+    path('edit/<int:id>', views.index, name='edit'),
+    path('create/', views.index, name='create'),
+    
     path('api/note/', views.api_notes.as_view(), name='api_notes'),
     path('api/note/<int:pk>/', views.api_note.as_view(), name='api_note'),
     path('api/tag/', views.api_tags.as_view(), name='api_tags'),
--- mysite/notes/views.py
+++ mysite/notes/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import Q
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from .models import *
 from .serializers import *
 
 
-def index(request):
+def index(request, **kwargs):
     if request.user.is_authenticated:
         return render(request, 'index.html')
     return render(request, 'landing.html')
+
+def shared(request, note_id):
+    note = get_object_or_404(Note, id=note_id, public=True)
+
+    return render(request, 'shared.html', context={'note': note})
 
 class api_notes(generics.ListCreateAPIView):
     serializer_class = NoteSerializer
